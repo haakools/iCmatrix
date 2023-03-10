e2a30718a74bfc0b29d4bc1ed74a20bb49d51bcc
Hakon Berggren <hakonberggren@gmail.com>
2023-03-10 18:10:40 +0100

    made main file and batch file make script with gcc

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* main */
+int main(int argc, char *argv[]){
+	printf("hello world");
+	return 0;
+}
+
---
+++ src/make.bat
@@ -0,0 +1,8 @@
+:: Makefile in batch script
+
+set "repoDir = %~dp0.."
+
+
+gcc %repoDir%src\main.c -o main.exe
+
+
